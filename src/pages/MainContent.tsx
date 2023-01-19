930e6c812901f8666173d9f6b3865f198ad44b3f
Malakai <kayydv1016@gmail.com>
2023-01-19 11:49:07 -0500

    fixed up dark/light mode gradients

--- src/pages/MainContent.tsx
+++ src/pages/MainContent.tsx
@@ -3,7 +3,7 @@ import '../styles/global_styles.css'
 import '../styles/component_styles.css'
 import { useThemeContext } from '../common/contexts/ThemeContext' ;
 import TestColorSwitcher from '../common/test/TestColorSwitcher' ;
-
+import TestTextContainer from '../common/test/TestTextContainer' ;
 
 function MainContent() {
     const { getTheme } = useThemeContext() ;
@@ -13,6 +13,7 @@ function MainContent() {
     return (
         <div className={'background page-container ' + theme}>
             <div className='content-container'>
+                <TestTextContainer/>
                 <TestColorSwitcher />
             </div>
         </div>
